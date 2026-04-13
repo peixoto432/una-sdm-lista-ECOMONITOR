0c6f09150fdd02218503f0a909ba2e8324344c28
peixoto432 <93793192+peixoto432@users.noreply.github.com>
2026-04-13 19:57:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using EcoMonitorApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+public partial class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        builder.Services.AddDbContext<AppDbContext>(static options =>
+            options.UseSqlite("Data Source=eco_monitor.db"));
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "EcoMonitor API v1");
+                c.RoutePrefix = string.Empty; // Define o Swagger como página inicial
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
